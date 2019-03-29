30b460268dac120b6bab24faba30f69c6351db7f
Devin Schmidt <dtschmidt@crimson.ua.edu>
2019-03-29 12:25:01 -0500

    3/29 end of day - did logic for quizzes. having issues with retrieving the entire quiz before moving on though

--- app/src/main/java/com/example/wabprojectredo/QuizActivity.kt
+++ app/src/main/java/com/example/wabprojectredo/QuizActivity.kt
@@ -1,8 +1,11 @@
 package com.example.wabprojectredo
 
+import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
+import com.example.wabprojectredo.GamesActivity.Companion.questionArray
 import com.example.wabprojectredo.classes.ChatMessage
 import com.example.wabprojectredo.classes.Question
 import com.google.firebase.database.ChildEventListener
@@ -21,71 +24,111 @@ class QuizActivity : AppCompatActivity() {
         1 corresponds with jan, 2 with feb, so on*/
         val months = arrayOf("null", "january", "february", "march", "april", "may", "june", "july", "august", "september", "october", "november", "december")
 
-        var questionArray = arrayOf<Question>()
-
         var questionNumber = 1
+
+        var questionsCorrect = 0
+
+        var totalQuestions = 0
+
+        var submittedYet = false
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz)
+        val questionArray = intent.getParcelableArrayExtra("questionArray")
 
 
-
-        listenForQuestions()
-
+   //     totalQuestions = questionList.size
+  //      aaa()
         //TODO: note that an array starts at 0, need to fix
         //TODO: this while loop goes through real quick and doesnt actually do anything
-        while (questionNumber < questionArray.size){
-            Log.d("QuizActivity", "um hi")
-            txtview_quiz_questionnum.text = "Question $questionNumber"
-            txtview_quiz_qtext.text = questionArray[questionNumber].actualQuestion
+    }
+/*
+    private fun aaa() {
+        if (questionNumber < questionList.size){
+            setUpPage()
+            Log.d("quiz", "uhhhh")
 
-            radio_quiz_1.text = questionArray[questionNumber].optionOne
-            radio_quiz_2.text = questionArray[questionNumber].optionTwo
-            radio_quiz_3.text = questionArray[questionNumber].optionThree
-            radio_quiz_4.text = questionArray[questionNumber].optionFour
+            if (submittedYet == false) {
+                btn_quiz_submitnext.setOnClickListener {
+                    //plays only if user has not submitted their answer for the current question yet
 
-            //TODO: do something if nothing is checked
-            btn_quiz_submitnext.setOnClickListener {
+                    highlightCorrectAnswer()
+                    highlightCurrentAnswerIfWrong()
 
+                    if (radio_quiz_1.isChecked && radio_quiz_1.text == questionList[questionNumber].correctAnswer) {
+                        questionsCorrect++
+                        submittedYet = true
+                    } else if (radio_quiz_2.isChecked && radio_quiz_2.text == questionList[questionNumber].correctAnswer) {
+                        questionsCorrect++
+                        submittedYet = true
+                    } else if (radio_quiz_3.isChecked && radio_quiz_3.text == questionList[questionNumber].correctAnswer) {
+                        questionsCorrect++
+                        submittedYet = true
+                    } else if (radio_quiz_4.isChecked && radio_quiz_4.text == questionList[questionNumber].correctAnswer) {
+                        questionsCorrect++
+                        submittedYet = true
+                    } else {
+                        Toast.makeText(this, "Please make a selection.", Toast.LENGTH_SHORT).show()
+                        return@setOnClickListener
+                    }
+                }
+            }
+
+            if (submittedYet == true) {
+                btn_quiz_submitnext.setOnClickListener {
+                    questionNumber++
+                    aaa()
+                }
             }
         }
     }
 
-    private fun listenForQuestions(){
-        val current = LocalDateTime.now()
-        val formatter = DateTimeFormatter.ofPattern("MM")
-        val formatted = current.format(formatter)
-        val currentMonth = months[formatted.toInt()]
+    private fun setUpPage() {
+        txtview_quiz_questionnum.text = "Question $questionNumber"
+        txtview_quiz_qtext.text = questionList[questionNumber].actualQuestion
 
-        val ref = FirebaseDatabase.getInstance().getReference("/quizzes/$currentMonth")
-
-        ref.addChildEventListener(object: ChildEventListener {
-            override fun onChildAdded(p0: DataSnapshot, p1: String?) {
-                val questionItem = p0.getValue(Question::class.java)
-
-                if (questionItem != null) {
-                    questionArray += questionItem
-
-                }
-            }
-
-            override fun onCancelled(p0: DatabaseError) {
-
-            }
-
-            override fun onChildChanged(p0: DataSnapshot, p1: String?) {
-
-            }
-
-            override fun onChildMoved(p0: DataSnapshot, p1: String?) {
-
-            }
-
-            override fun onChildRemoved(p0: DataSnapshot) {
-
-            }
-        })
+        radio_quiz_1.text = questionList[questionNumber].optionOne
+        radio_quiz_2.text = questionList[questionNumber].optionTwo
+        radio_quiz_3.text = questionList[questionNumber].optionThree
+        radio_quiz_4.text = questionList[questionNumber].optionFour
     }
+
+    //also changes color of submit button
+    private fun highlightCurrentAnswerIfWrong() {
+        if (radio_quiz_1.isChecked && radio_quiz_1.text != questionList[questionNumber].correctAnswer){
+            btn_quiz_submitnext.setBackgroundColor(Color.BLUE)
+            radio_quiz_1.setBackgroundColor(Color.RED)
+        }
+        else if (radio_quiz_2.isChecked && radio_quiz_2.text != questionList[questionNumber].correctAnswer){
+            btn_quiz_submitnext.setBackgroundColor(Color.BLUE)
+            radio_quiz_2.setBackgroundColor(Color.RED)
+        }
+        else if (radio_quiz_3.isChecked && radio_quiz_3.text != questionList[questionNumber].correctAnswer){
+            btn_quiz_submitnext.setBackgroundColor(Color.BLUE)
+            radio_quiz_3.setBackgroundColor(Color.RED)
+        }
+        else if (radio_quiz_4.isChecked && radio_quiz_4.text != questionList[questionNumber].correctAnswer){
+            btn_quiz_submitnext.setBackgroundColor(Color.BLUE)
+            radio_quiz_4.setBackgroundColor(Color.RED)
+        }
+    }
+
+    private fun highlightCorrectAnswer() {
+        if (radio_quiz_1.text == questionList[questionNumber].correctAnswer){
+            radio_quiz_1.setBackgroundColor(Color.GREEN)
+        }
+        else if (radio_quiz_2.text == questionList[questionNumber].correctAnswer){
+            radio_quiz_2.setBackgroundColor(Color.GREEN)
+        }
+        else if (radio_quiz_3.text == questionList[questionNumber].correctAnswer){
+            radio_quiz_3.setBackgroundColor(Color.GREEN)
+        }
+        else if (radio_quiz_4.text == questionList[questionNumber].correctAnswer){
+            radio_quiz_4.setBackgroundColor(Color.GREEN)
+        }
+    }
+*/
+
 }
