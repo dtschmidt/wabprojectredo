3168b8341993ffe8e5a4174c3cd3ce4223e943ce
Devin Schmidt <dtschmidt@crimson.ua.edu>
2019-04-02 15:36:00 -0500

    4/2 end of day - work on homepage ui

--- app/src/main/java/com/example/wabprojectredo/GamesActivity.kt
+++ app/src/main/java/com/example/wabprojectredo/GamesActivity.kt
@@ -9,6 +9,8 @@ import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import kotlinx.android.synthetic.main.activity_games.*
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
@@ -16,7 +18,7 @@ import java.time.format.DateTimeFormatter
 class GamesActivity : AppCompatActivity() {
 
     companion object {
-        var questionArray = arrayOf<Question>()
+
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,50 +28,11 @@ class GamesActivity : AppCompatActivity() {
         //TODO: listen for questions needs to run all the way through before adding the extra.
         btn_games_quiz.setOnClickListener {
 
-            listenForQuestions()
-
             val intent = Intent(this, QuizActivity::class.java)
-            intent.putExtra("questionArray", questionArray)
             startActivity(intent)
 
         }
     }
 
-    private fun listenForQuestions(){
 
-        val current = LocalDateTime.now()
-        val formatter = DateTimeFormatter.ofPattern("MM")
-        val formatted = current.format(formatter)
-        val currentMonth = QuizActivity.months[formatted.toInt()]
-
-        val ref = FirebaseDatabase.getInstance().getReference("/quizzes/$currentMonth")
-
-        ref.addChildEventListener(object: ChildEventListener {
-            override fun onChildAdded(p0: DataSnapshot, p1: String?) {
-                val questionItem = p0.getValue(Question::class.java)
-
-                if (questionItem != null) {
-                    questionArray += questionItem
-
-                }
-            }
-
-            override fun onCancelled(p0: DatabaseError) {
-
-            }
-
-            override fun onChildChanged(p0: DataSnapshot, p1: String?) {
-
-            }
-
-            override fun onChildMoved(p0: DataSnapshot, p1: String?) {
-
-            }
-
-            override fun onChildRemoved(p0: DataSnapshot) {
-
-            }
-        })
-
-    }
 }
--- app/src/main/java/com/example/wabprojectredo/QuizActivity.kt
+++ app/src/main/java/com/example/wabprojectredo/QuizActivity.kt
@@ -5,7 +5,6 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
-import com.example.wabprojectredo.GamesActivity.Companion.questionArray
 import com.example.wabprojectredo.classes.ChatMessage
 import com.example.wabprojectredo.classes.Question
 import com.google.firebase.database.ChildEventListener
@@ -13,6 +12,7 @@ import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import kotlinx.android.synthetic.main.activity_quiz.*
+import kotlinx.coroutines.*
 import java.time.LocalDateTime
 import java.time.YearMonth
 import java.time.format.DateTimeFormatter
@@ -20,6 +20,9 @@ import java.time.format.DateTimeFormatter
 class QuizActivity : AppCompatActivity() {
 
     companion object {
+
+        var questionArray = arrayOf<Question>()
+
         /*used to get current month in listenForQuestions; first place is null so that
         1 corresponds with jan, 2 with feb, so on*/
         val months = arrayOf("null", "january", "february", "march", "april", "may", "june", "july", "august", "september", "october", "november", "december")
@@ -38,15 +41,23 @@ class QuizActivity : AppCompatActivity() {
         setContentView(R.layout.activity_quiz)
         val questionArray = intent.getParcelableArrayExtra("questionArray")
 
+        main()
 
-   //     totalQuestions = questionList.size
-  //      aaa()
+
         //TODO: note that an array starts at 0, need to fix
         //TODO: this while loop goes through real quick and doesnt actually do anything
     }
-/*
-    private fun aaa() {
-        if (questionNumber < questionList.size){
+
+    fun main() = runBlocking{
+        listenForQuestions()
+        aaa()
+    }
+
+     private fun aaa() {
+
+
+        totalQuestions = questionArray.size
+        if (questionNumber < questionArray.size){
             setUpPage()
             Log.d("quiz", "uhhhh")
 
@@ -57,16 +68,16 @@ class QuizActivity : AppCompatActivity() {
                     highlightCorrectAnswer()
                     highlightCurrentAnswerIfWrong()
 
-                    if (radio_quiz_1.isChecked && radio_quiz_1.text == questionList[questionNumber].correctAnswer) {
+                    if (radio_quiz_1.isChecked && radio_quiz_1.text == questionArray[questionNumber].correctAnswer) {
                         questionsCorrect++
                         submittedYet = true
-                    } else if (radio_quiz_2.isChecked && radio_quiz_2.text == questionList[questionNumber].correctAnswer) {
+                    } else if (radio_quiz_2.isChecked && radio_quiz_2.text == questionArray[questionNumber].correctAnswer) {
                         questionsCorrect++
                         submittedYet = true
-                    } else if (radio_quiz_3.isChecked && radio_quiz_3.text == questionList[questionNumber].correctAnswer) {
+                    } else if (radio_quiz_3.isChecked && radio_quiz_3.text == questionArray[questionNumber].correctAnswer) {
                         questionsCorrect++
                         submittedYet = true
-                    } else if (radio_quiz_4.isChecked && radio_quiz_4.text == questionList[questionNumber].correctAnswer) {
+                    } else if (radio_quiz_4.isChecked && radio_quiz_4.text == questionArray[questionNumber].correctAnswer) {
                         questionsCorrect++
                         submittedYet = true
                     } else {
@@ -87,48 +98,86 @@ class QuizActivity : AppCompatActivity() {
 
     private fun setUpPage() {
         txtview_quiz_questionnum.text = "Question $questionNumber"
-        txtview_quiz_qtext.text = questionList[questionNumber].actualQuestion
+        txtview_quiz_qtext.text = questionArray[questionNumber].actualQuestion
 
-        radio_quiz_1.text = questionList[questionNumber].optionOne
-        radio_quiz_2.text = questionList[questionNumber].optionTwo
-        radio_quiz_3.text = questionList[questionNumber].optionThree
-        radio_quiz_4.text = questionList[questionNumber].optionFour
+        radio_quiz_1.text = questionArray[questionNumber].optionOne
+        radio_quiz_2.text = questionArray[questionNumber].optionTwo
+        radio_quiz_3.text = questionArray[questionNumber].optionThree
+        radio_quiz_4.text = questionArray[questionNumber].optionFour
     }
 
     //also changes color of submit button
     private fun highlightCurrentAnswerIfWrong() {
-        if (radio_quiz_1.isChecked && radio_quiz_1.text != questionList[questionNumber].correctAnswer){
+        if (radio_quiz_1.isChecked && radio_quiz_1.text != questionArray[questionNumber].correctAnswer){
             btn_quiz_submitnext.setBackgroundColor(Color.BLUE)
             radio_quiz_1.setBackgroundColor(Color.RED)
         }
-        else if (radio_quiz_2.isChecked && radio_quiz_2.text != questionList[questionNumber].correctAnswer){
+        else if (radio_quiz_2.isChecked && radio_quiz_2.text != questionArray[questionNumber].correctAnswer){
             btn_quiz_submitnext.setBackgroundColor(Color.BLUE)
             radio_quiz_2.setBackgroundColor(Color.RED)
         }
-        else if (radio_quiz_3.isChecked && radio_quiz_3.text != questionList[questionNumber].correctAnswer){
+        else if (radio_quiz_3.isChecked && radio_quiz_3.text != questionArray[questionNumber].correctAnswer){
             btn_quiz_submitnext.setBackgroundColor(Color.BLUE)
             radio_quiz_3.setBackgroundColor(Color.RED)
         }
-        else if (radio_quiz_4.isChecked && radio_quiz_4.text != questionList[questionNumber].correctAnswer){
+        else if (radio_quiz_4.isChecked && radio_quiz_4.text != questionArray[questionNumber].correctAnswer){
             btn_quiz_submitnext.setBackgroundColor(Color.BLUE)
             radio_quiz_4.setBackgroundColor(Color.RED)
         }
     }
 
     private fun highlightCorrectAnswer() {
-        if (radio_quiz_1.text == questionList[questionNumber].correctAnswer){
+        if (radio_quiz_1.text == questionArray[questionNumber].correctAnswer){
             radio_quiz_1.setBackgroundColor(Color.GREEN)
         }
-        else if (radio_quiz_2.text == questionList[questionNumber].correctAnswer){
+        else if (radio_quiz_2.text == questionArray[questionNumber].correctAnswer){
             radio_quiz_2.setBackgroundColor(Color.GREEN)
         }
-        else if (radio_quiz_3.text == questionList[questionNumber].correctAnswer){
+        else if (radio_quiz_3.text == questionArray[questionNumber].correctAnswer){
             radio_quiz_3.setBackgroundColor(Color.GREEN)
         }
-        else if (radio_quiz_4.text == questionList[questionNumber].correctAnswer){
+        else if (radio_quiz_4.text == questionArray[questionNumber].correctAnswer){
             radio_quiz_4.setBackgroundColor(Color.GREEN)
         }
     }
-*/
+
+    private fun listenForQuestions(){
+
+        val current = LocalDateTime.now()
+        val formatter = DateTimeFormatter.ofPattern("MM")
+        val formatted = current.format(formatter)
+        val currentMonth = QuizActivity.months[formatted.toInt()]
+
+        val ref = FirebaseDatabase.getInstance().getReference("/quizzes/$currentMonth")
+
+        ref.addChildEventListener(object: ChildEventListener {
+            override fun onChildAdded(p0: DataSnapshot, p1: String?) {
+                val questionItem = p0.getValue(Question::class.java)
+
+                if (questionItem != null) {
+                    questionArray += questionItem
+
+                }
+            }
+
+            override fun onCancelled(p0: DatabaseError) {
+
+            }
+
+            override fun onChildChanged(p0: DataSnapshot, p1: String?) {
+
+            }
+
+            override fun onChildMoved(p0: DataSnapshot, p1: String?) {
+
+            }
+
+            override fun onChildRemoved(p0: DataSnapshot) {
+
+            }
+        })
+        return
+    }
+
 
 }
